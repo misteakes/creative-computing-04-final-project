c21f5065919d761607780e4504bd5f639eaf66f1
Misty <misty@Mistys-MBP.fios-router.home>
2018-11-26 21:49:44 -0500

    ?

--- js/site.js
+++ js/site.js
@@ -59,24 +59,27 @@ function hslColor(h, s, l) {
 
 /* START WRITING YOUR CODE BELOW */
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "100%";  // IE10
+        elements[i].style.flex = "100%";
+    }
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsbyClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length};
-  for (i=0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+// Four images side by side
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";  // IE10
+        elements[i].style.flex = "25%";
+    }
 }
-
 
 
 
